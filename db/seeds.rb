666d576b3ad764aa81d000e9026098fc7099aabb
Jonas <jonas2moreira@hotmail.com>
2013-11-21 22:01:57 -0200

    seeds modificado

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+  User.create(name: "Jonas Moreira",
+                 email: "jonas2moreira@ig.com",
+                 password: "foobar",
+                 password_confirmation: "foobar",
+                   admin: true)
